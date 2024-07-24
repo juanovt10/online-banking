0ffeb517e8f2b51d9095d26ae378dc0c1cacfbb4
Juan Velasquez <juanovelasquez10@gmail.com>
2024-07-24 18:40:18 +0200

    Implement desktop navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,39 @@
+
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
-
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: 'Juan', lastName: 'Velasquez' };
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className='flex h-screen w-full font-inter'>
+      <Sidebar
+        user={loggedIn}
+      />
+      <div className='flex size-full flex-col'>
+        <div className='root-layout'>
+          <Image 
+            src='/icons/logo.svg'
+            width={30}
+            height={30}
+            alt='menu icon'
+          />
+          <div>
+            <MobileNav 
+              user={loggedIn}
+            />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
